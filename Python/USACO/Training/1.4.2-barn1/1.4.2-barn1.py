c3931b06ccf02c389ccb75638d8db10cc12a0691
MehvixDesktop <mehvix@protonmail.com>
2019-12-29 11:47:42 -0600

    finished usaco 1.4.2

--- Python/USACO/Training/1.4.2-barn1/1.4.2-barn1.py
+++ Python/USACO/Training/1.4.2-barn1/1.4.2-barn1.py
@@ -9,23 +9,31 @@ Dec 9, 2019
 fin = open('barn1.in', 'r')
 fout = open('barn1.out', 'w')
 
-
 max_boards, stalls, cows = list(map(int, fin.readline().strip().split()))
 
 stall_gaps = []
 last_stall = -1
-for pos in range(cows):
-    cur_stall = int(fin.readline().strip())
+cow_list = []
 
-    if pos != 0:
-        if not (last_stall == cur_stall -1):
-            gap = cur_stall-last_stall
+
+for pos in range(cows):
+    cow_list.append(int(fin.readline().strip()))
+
+cow_list.sort()
+print(cow_list)
+
+for cur_stall in cow_list:
+    if last_stall != -1:
+        if not (last_stall == cur_stall - 1):
+            gap = cur_stall - last_stall - 1
             print('gap between {} / {} of {}'.format(cur_stall, last_stall, gap))
             stall_gaps.append(gap)
-        else:
-            print('no gap between {} / {}'.format(cur_stall, last_stall))
-
     last_stall = cur_stall
 
-print(stall_gaps)
-# fout.write(FINAL + '\n')
+stall_gaps.sort()
+stall_gaps.reverse()
+stall_gaps = stall_gaps[(max_boards - 1):]
+
+FINAL = sum(stall_gaps) + cows
+print(FINAL)
+fout.write(str(FINAL) + '\n')
